8786c3698863dfaf32b9f2f0dd2ceef7d54610b1
SamuelOgbonnaeze <48336193+SamuelOgbonnaeze@users.noreply.github.com>
2024-03-21 18:04:48 +0100

    Update Rooms.jsx

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Rooms = () => {
     return (
-        <div>
+        <div className="mb-10">
             <div className='h-[700px] sm:h-[850px] md:h-[1050px] lg:h-[950px]'>
                 <div className='max-w-[1400px] h-[500px] bg-blue-100 mx-auto my-20 pt-16 lg:mb-[20%] md: mb-[35%] px-4 grid lg:grid-cols-3 gap-4'>
                     <div className='lg:top-20 relative lg:col-span-1 col-span-2'>
